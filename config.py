2aa16981adcab156d38c47a4d88e4dde7e9bd4d6
maxi <xmaxiverax@gmail.com>
2025-07-12 15:44:54 -0300

    little changes

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 TWITTER_USER = "your_user" #your twitter username (WITHOUT @)
 TWITTER_PASS = "your_password" # Your Twitter password
-TARGET_USER = "mauriciomacri" #username of the user whose likes you want to scrape (WITHOUT @)
+TARGET_USER = "your_target_user" #username of the user whose likes you want to scrape (WITHOUT @)
 TWEET_LIMIT = 800 # Maximum number of tweets to scrape from the target user's likes. 
 
 
